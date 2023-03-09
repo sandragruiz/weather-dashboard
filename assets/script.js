0103687e4a8c872f534d71b3315eb39d9e749c6b
unknown <sandra.gutierrez.ru@gmail.com>
2023-03-08 18:18:10 -0600

    added forecast header, fixed HTML card info

--- assets/script.js
+++ assets/script.js
@@ -1,15 +1,20 @@
 // Variables
+
 var apiKEY = "469693915225b9c0f0a6990cf2097538"
 var submitBtn = document.getElementById("submit-btn")
 
 //Event Listener for Submit Button
+
 submitBtn.addEventListener("click",function(event){
     event.preventDefault()
+    document.getElementById("forecast-header").value="5-Day Forecast";
     var city = document.getElementById("city-name").value
+    localStorage.setItem("city name", city);  //save search in local storage
     get5dayForecast(city)
 })
 
 //Get weather data from OpenWeather API
+
 function get5dayForecast(city){
     var URL = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKEY}&units=imperial`
     fetch(URL)
@@ -18,23 +23,22 @@ function get5dayForecast(city){
         console.log(apiData)
         var htmlCards = ""
         for(let i=0;i<apiData.list.length;i=i+8) // 24/3 = 8
-        {
+        {  
             htmlCards += `
             <div class="card" style="width: 18rem;">
-            <p> Temperature: ${apiData.list[i].main.temp}
-  <img src="http://openweathermap.org/img/wn/${apiData.list[i].weather[0].icon}@2x.png" class="card-img-top" alt="icon."></p>
+            <h5 class="card-title">${apiData.list[i].dt_txt}</h5>
+  <img src="http://openweathermap.org/img/wn/${apiData.list[i].weather[0].icon}@2x.png" class="card-img-top" alt="icon"></p>
   <div class="card-body">
-    <h5 class="card-title">${apiData.list[i].dt_txt}</h5>
-    <p class="card-text">Description: ${apiData.list[i].weather[0].description}.</p>
-    <p class="card-text">DEscription: ${apiData.list[i].main.humidity}.</p>
-    <p class="card-text">DEscription: ${apiData.list[i].wind.speed}.</p>
+    <p class="card-text">Conditions: ${apiData.list[i].weather[0].description}</p>
+    <p class="card-text">Temperature: ${apiData.list[i].main.temp} F</p>
+    <p class="card-text">Humidity: ${apiData.list[i].main.humidity}%</p>
+    <p class="card-text">Wind speed: ${apiData.list[i].wind.speed} mph</p>
 
   </div>
 </div>
-            `
-
-           
+            `   
         }
         document.getElementById("five-day").innerHTML=htmlCards
     })
 }
+
